e66e4595e6fdf62814b48b2f807d1b3f0d338d6f
Barbosa Thaíssa <thathamaria03@gmail.com>
2022-11-29 03:31:22 -0300

    Parte 2 de loops completa

--- js/scripts.js
+++ js/scripts.js
@@ -126,36 +126,51 @@ const estudantes = [
   },
 ];
 
-const parcelarCurso = (parcela) => {
+const carrinhoCursos = [500, 900, 400];
+
+const parcelarCurso = (arr, parcela) => {
   //Ter 20% para pg avista ou em 2 vezes
+  let resultado;
 
-  let valorTotal = cursos[2].valor;
-  let valorParcela = valorTotal / parcela;
-
+  let total1 = arr[0];
+  let valor2 = arr[0] + arr[1];
+  let valor3 = arr[0] + arr[1] + arr[2];
+  let desconto2cur = (valor2 * 10) / 100;
+  let desconto3cur = (valor3 * 15) / 100;
+  
+  let total2 = valor2 - desconto2cur;
+  let total3 = valor3 - desconto3cur;
+  
+  if (arr.length === 1) {
+    resultado = total1;
+  } else if (arr.length === 2) {
+    resultado = total2;
+  } else if (arr.length === 3) {
+    resultado = total3;
+  } else {
+    resultado = `Não foi possivel calcular`;
+  }
+  
+  let valorParcela = resultado / parcela;
+  
   if (parcela === 2 || parcela === 1) {
-    //retorna o valor do curso com desconto
+    let desconto = (resultado * 20) / 100;
+    resultado = resultado - desconto;
+    valorParcela = resultado / parcela;
 
-    let desconto = (valorTotal * 20) / 100;
-    valorTotal -= desconto;
-    valorParcela = valorTotal / parcela;
-
-    return `O curso ${
-      cursos[2].curso
-    } ficou no valor total de ${valorTotal}. Em ${parcela}x de ${valorParcela.toFixed(
+    return `O valor do pagamento é de R$ ${resultado}. Em ${parcela}x de ${valorParcela.toFixed(
       2
     )} reais. Foi concedido um desconto de 20%.`;
   } else if (parcela >= 3 && parcela <= 12) {
-    //retorna o valor do curso sem desconto
-
-    return `O curso ${
-      cursos[2].curso
-    } ficou no valor total de R$ ${valorTotal}. Em ${parcela}x de ${valorParcela.toFixed(
+    return `O valor do pagamento é de R$ ${resultado}. Em ${parcela}x de ${valorParcela.toFixed(
       2
     )} reais.`;
   } else {
     return "Desculpe, não foi possível calcular o valor! Por favor tente um novamente";
   }
 };
+
+console.log(parcelarCurso(carrinhoCursos, 2));
 
 const buscarCurso = (nameCurso, cur) => {
   //uma busca pelo array de cursos, retornar o objeto referente ao curso
@@ -241,3 +256,21 @@ console.log(buscarCurso("JavaScript", cursos));
 console.log(buscarEstudante("Halle Berry", estudantes));
 console.log(buscarTurma("Sibyla", turmas));
 
+const matricular = (nome, curso, turma, nParcelas) => {
+  //inseri um aluno no array de estudantes. mostrar array de estudantes no console
+  estudantes.push({
+    estudante: nome,
+    turma: turma,
+    curso: curso,
+    numParcelas: nParcelas,
+  });
+
+  console.log(estudantes);
+
+  console.log(`Aluno Matriculado
+  Nome: ${nome}
+  Curso: ${curso}
+  Turma: ${turma}`);
+};
+
+matricular("Calvin Klein", "JavaScript", "Clarke", 3);
